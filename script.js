70b161e33ef9bfe5c63f3daade0dc27cb21b01f3
Daniil10Sh <danik2022odessa@gmail.com>
2024-02-27 20:19:17 +0200

    add script

--- script.js
+++ script.js
@@ -7,34 +7,34 @@ console.log (whichmonth);
  const p = document.querySelector('.show-massedge');
  console.log (p);
 
- function handlButtonclick{} (
+ function handlButtonclick() {
     console.log ('hi');
     const year = monthes.value;
 
     switch(year){ 
-        case "january":
-        case "february":
+        case "1":
+        case "2":
             alert('winter');
-        case "march":
-        case "april":
-        case "march":
+        case "3":
+        case "4":
+        case "5":
             alert('Spring');
-        case "june":
-        case "juli":
-        case "august":
+        case "6":
+        case "7":
+        case "8":
             alert('Summer');
-        case "september":
-        case "october":
-        case "november":
+        case "9":
+        case "10":
+        case "11":
             alert('Autumn');
-        case "december":
+        case "12":
             alert('Winter');
          break;
          default:
          alert('it is not a month ')
          break;
     }
-)
+}
 
 whichmonth.addEventListener('click', handlButtonclick);
 
